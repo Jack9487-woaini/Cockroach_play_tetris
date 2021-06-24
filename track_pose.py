682aef5c0a6af58f7757f41715b32014245da1b8
Jack9487-woaini <72640027+Jack9487-woaini@users.noreply.github.com>
2021-06-24 16:39:25 +0800

    Update track_pose.py

--- track_pose.py
+++ track_pose.py
@@ -6,33 +6,23 @@ import pyautogui as pyau
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
-# Load the model
+# 載入模型
 path ="keras_model.h5"
 path = path.encode(encoding="utf-8")
 model = tensorflow.keras.models.load_model(path)
 
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1.
 
 
-
-def cv2_to_pil(img): #Since you want to be able to use Pillow (PIL)
+def cv2_to_pil(img): # 把 opencv 畫面轉給 pil
     return Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
 
-cam = cv2.VideoCapture(0) #Define the camera, 0 is which camera, if you have more than 1
-ret_val, img = cam.read() #cam.read() returns ret (0/1 if the camera is working) and img, 
-#the actual image of the camera in a numpy array
-
-#0 to X milliseconds to stop for a certain amount of time.
-
- #convert the image to PIL so you can use it that way.
-# Replace this with the path to your image
+cam = cv2.VideoCapture(0) # 打開攝影機
+ret_val, img = cam.read() # 畫面讀取
 
 
-#resize the image to a 224x224 with the same strategy as in TM2:
-#resizing the image to be at least 224x224 and then cropping from the center
+
+
 
 
 
@@ -43,23 +33,26 @@ while True:
     # 繼續抓畫面
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
     ok, img = cam.read()
-    # display the resized image
+    
     pil_img = cv2_to_pil(img)
     image = pil_img
     size = (224, 224)
+    # 調整照片大小
     image = ImageOps.fit(image, size, Image.ANTIALIAS)
     image_array = np.asarray(image)
-# Normalize the image
+    # 標準化圖片
     normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
-    # Load the image into the array
+    # 把照片放到 array
     data[0] = normalized_image_array
     #print(data[0])
-    # run the inference
+    
+    # 讓模型辨識
     prediction = model.predict(data)
     score = prediction.tolist()
     score =[score[0][0],score[0][1],score[0][2],score[0][3],score[0][4]]
     count += 1
+    # 看誰機率最高
     if score.index(max(score)) == 0 and count >= 5:
         
         #pyau.press('up')
