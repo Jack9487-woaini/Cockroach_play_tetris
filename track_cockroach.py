5394770719d93647a2dd4c4aa5b8a2bf04885542
Jack9487-woaini <72640027+Jack9487-woaini@users.noreply.github.com>
2021-06-24 16:33:47 +0800

    Update track_cockroach.py

--- track_cockroach.py
+++ track_cockroach.py
@@ -1,58 +1,54 @@
 import cv2
 import sys
-import keyboard
-from pymouse import PyMouse
 import pyautogui
 def target_tracking(tracker):
-    # Read video
+    # 0 會抓第一台攝影機 ，如果是放影片路徑，就會去抓影片。
     video = cv2.VideoCapture(0)
     m = PyMouse()     
-    # Exit if video not opened.
+    # 檢查攝影機有沒有啟動
     if not video.isOpened():
         print("Could not open video")
         sys.exit()
  
-    # Read first frame.
+    # 抓影片的第一幀
     ok, frame = video.read()
     if not ok:
         print('Cannot read video file')
         sys.exit()
-    
-    # Define an initial bounding box
-    bbox = (287, 23, 86, 320)
  
-    # Uncomment the line below to select a different bounding box
+    # Opencv 會跑出一個視窗，讓你去框出你要的區塊
     bbox = cv2.selectROI(frame, False)
  
-    # Initialize tracker with first frame and bounding box
+    # 初始化 tracker ，bbox 剛框出的區塊，frame 第一幀
     ok = tracker.init(frame, bbox)
     count = 0
     x = 0
     y = 0
     while True:
-        # Read a new frame
+       # 繼續抓畫面
         ok, frame = video.read()
         if not ok:
             break
         
-        # Start timer
+        # 計算時間
         timer = cv2.getTickCount()
  
-        # Update tracker
+       # 讓tracker 去抓現在這一幀的畫面
         ok, bbox = tracker.update(frame)
  
-        # Calculate Frames per second (FPS)
+        # 計算 fps
         fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
  
-        # Draw bounding box
+        
         if ok:
-            # Tracking success
+            # 追蹤成功
             p1 = (int(bbox[0]), int(bbox[1]))
             p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
             cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
         else :
-            # Tracking failure
+            # 追蹤失敗
             cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+        # 現在小寵物在的區塊
         cv2.putText(frame, "direction : " , (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
         if count == 0:
             x = int(bbox[0])
@@ -62,92 +58,38 @@ def target_tracking(tracker):
             y = int(bbox[1])
             if y >= 90 and y <=157:
                 pyautogui.press('up')
-                #keyboard.press_and_release('w') 
-                #keyboard.press_and_release('up') 
+
                 cv2.putText(frame, "direction : " + "up", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
                 
             elif y >= 158 and y <= 224:
                 pyautogui.press('left')
-                #keyboard.press_and_release('a') 
-                #keyboard.press_and_release('left') 
+
                 cv2.putText(frame, "direction : " + "left", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
                 
             elif y >= 225 and y <= 292:
                 pyautogui.press('right')
-                #keyboard.press_and_release('d')
-                #keyboard.press_and_release('right') 
+
                 cv2.putText(frame, "direction : " + "right", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
             elif y >= 293 and y <= 360:
                 pyautogui.press('up')
-                #keyboard.press_and_release('up')
-                #m_pos = m.position()
-                #m.click(m_pos[0],m_pos[1],2)
-                #pos = pyautogui.position()
-                #pyautogui.mouseDown(button='right')
+
                 cv2.putText(frame, "direction : " + "down", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
                 
             count = 0
-        if count == -3:
-            # tmpx = x - int(bbox[0])
-            # tmpy = y - int(bbox[1])
-            tmpx1 = 0 + int(bbox[0])
-            tmpy1 = 0 + int(bbox[1])
-            tmpx2 = 480 - int(bbox[0])
-            tmpy2 = 480 - int(bbox[1])
-            winx = [0,0]
-            winy = [0,0]
-            if abs(tmpx1) < abs(tmpx2):
-                winx[1] = abs(tmpx1)
-            else :
-                winx[0] = 1
-                winx[1] = abs(tmpx2)
-                
-            if abs(tmpy1) < abs(tmpy2):
-                winy[1]  = abs(tmpy1)
-            else :
-                winy[0] = 1
-                winy[1]  = abs(tmpy2)
-            
-            
-            
-#             if winx[1] > winy[1] and winx[0] == 0 :
-#                 keyboard.press_and_release('left') 
-#                 cv2.putText(frame, "direction : " + "left", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-#             elif winx[1] > winy[1] and winx[0] == 1 :
-#                 keyboard.press_and_release('right') 
-#                 cv2.putText(frame, "direction : " + "right", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-#             if winy[1] > winx[1] and winy[0] == 0  :
-#                 keyboard.press_and_release('up') 
-#                 cv2.putText(frame, "direction : " + "up", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-#             elif winy[1] > winx[1] and winy[0] == 1:
-#                 keyboard.press_and_release('up') 
-#                 cv2.putText(frame, "direction : " + "down", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-            # if abs(tmpx) > abs(tmpy) and tmpx > 0 and abs(tmpx) > 5 :
-            
-                # cv2.putText(frame, "direction : " + "right", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-                
-            # if abs(tmpx) > abs(tmpy) and tmpx < 0 and abs(tmpx) > 5:
-            
-                # cv2.putText(frame, "direction : " + "left", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-            # if abs(tmpy) > abs(tmpx) and tmpy > 0 and abs(tmpy) > 10:
-            
-                # cv2.putText(frame, "direction : " + "up", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-            # if abs(tmpy) > abs(tmpx) and tmpy < 0 and abs(tmpy) > 10:
-                # cv2.putText(frame, "direction : " + "down", (100,90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-            count = 0
+
         count += 1
-        # Display tracker type on frame
+        # 現在使用的 traker 類型
         cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
     
-        # Display FPS on frame
+        # 現在畫面幀數
         cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
-        
+        # 框框的座標，跟高寬。
         cv2.putText(frame, "box : " + str(int(bbox[0]))+","+str(int(bbox[1]))+","+str(int(bbox[2]))+","+str(int(bbox[3])), (100,70), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
         
-        # Display result
+        # 跑出攝影機畫面的視窗
         cv2.imshow("Tracking", frame)
         cv2.resizeWindow("Tracking", 600, 480);
-        # Exit if ESC pressed
+        # 按 esc 退出視窗
         k = cv2.waitKey(1) & 0xff
         if k == 27 : 
             break
@@ -158,7 +100,7 @@ if __name__ == '__main__' :
     # Instead of MIL, you can also use
  
     tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'MOSSE', 'CSRT']
-    tracker_type = tracker_types[6]
+    tracker_type = tracker_types[6] # 選擇使用的追蹤函數 type
  
  
     if tracker_type == 'BOOSTING':
